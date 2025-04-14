48ebdd2ef8afdcf44c5b5694b46b7976ef23b10c
G9 <69814860+avik2999@users.noreply.github.com>
2025-04-14 12:30:50 +0530

    Add files via upload

---
+++ OP.bat
@@ -0,0 +1,18 @@
+echo
+cls
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+Del /S /F /Q %Windir%\Temp
+Del /S /F /Q %temp%
+del /s /f /q C:\ProgramData\Tencent
+del /s /f /q C:\Users%USERNAME%\AppData\Local\Tencent
+del /s /f /q C:\Users%USERNAME%\AppData\Roaming\Tencent
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\367"
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\367.ini"
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\30"
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\30.ini"
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\368"
+del "C:\Program Files\TxGameAssistant\AOW_Rootfs_100\0\368.ini"
+del /s/f/q C:\\Users\\ % USERNAME % \\AppData\\Local\\Tencent
+del /s/f/q C:\\Users\ % USERNAME % \\AppData\\Roaming\\Tencent
+del /s/f/q C:\\ProgramData\\Tencent
